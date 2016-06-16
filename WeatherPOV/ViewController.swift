81dc743344901fd14b4ffb967777611089220783
Leo <superartstudio@gmail.com>
2016-06-16 21:22:55 +1000

    initial commit

---
+++ WeatherPOV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherPOV
+//
+//  Created by Leo on 16/06/2016.
+//  Copyright © 2016 Super Art Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
